9883d17200402bdbcd3037a7c759687e30ace8c9
Francois Botha <botha.francois@gmail.com>
2026-04-11 19:15:23 +1000

    [JOBS-002] Expose jobs.* IPC API via main-process handlers and preload bridge

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 const path = require('path')
+const jobManager = require('../../.ombutocode/src/src/main/jobManager')
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -22,6 +23,31 @@ function createWindow () {
 ipcMain.handle('dropsync:getAppVersion', () => {
   return app.getVersion()
 })
+
+function toIpcResponse (handler) {
+  return async (...args) => {
+    try {
+      const data = await handler(...args)
+      return { ok: true, data }
+    } catch (error) {
+      return {
+        ok: false,
+        error: error instanceof Error ? error.message : String(error)
+      }
+    }
+  }
+}
+
+ipcMain.handle('jobs.list', toIpcResponse(() => jobManager.listJobs()))
+ipcMain.handle('jobs.get', toIpcResponse((_event, id) => jobManager.getJob(id)))
+ipcMain.handle('jobs.create', toIpcResponse((_event, job) => jobManager.createJob(job)))
+ipcMain.handle('jobs.update', toIpcResponse((_event, id, fields) => jobManager.updateJob(id, fields)))
+ipcMain.handle('jobs.delete', toIpcResponse((_event, id) => jobManager.deleteJob(id)))
+ipcMain.handle('jobs.toggleEnabled', toIpcResponse((_event, id) => jobManager.toggleJobEnabled(id)))
+ipcMain.handle('jobs.pickSourceFolder', toIpcResponse(async () => {
+  const result = await dialog.showOpenDialog({ properties: ['openDirectory'] })
+  return result.canceled ? null : result.filePaths[0]
+}))
 
 app.whenReady().then(createWindow)
 
--- src/preload/index.js
+++ src/preload/index.js
@@ -4,3 +4,15 @@ contextBridge.exposeInMainWorld('dropsync', {
   ping: () => 'pong',
   getAppVersion: () => ipcRenderer.invoke('dropsync:getAppVersion')
 })
+
+contextBridge.exposeInMainWorld('api', {
+  jobs: {
+    list: () => ipcRenderer.invoke('jobs.list'),
+    get: (id) => ipcRenderer.invoke('jobs.get', id),
+    create: (job) => ipcRenderer.invoke('jobs.create', job),
+    update: (id, fields) => ipcRenderer.invoke('jobs.update', id, fields),
+    delete: (id) => ipcRenderer.invoke('jobs.delete', id),
+    toggleEnabled: (id) => ipcRenderer.invoke('jobs.toggleEnabled', id),
+    pickSourceFolder: () => ipcRenderer.invoke('jobs.pickSourceFolder')
+  }
+})
